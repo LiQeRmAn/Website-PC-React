da5068ddf59b62a26b9cc867bea16092a763b6ed
LiQeRmAn <evgeniy.bodrov.2005@mail.ru>
2025-06-16 00:30:24 +0300

    Added Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-light py-4 mt-5">
+      <div className="text-center">
+        <p className="mb-0">© 2025 MRN. Все права защищены.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
